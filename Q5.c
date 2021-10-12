2534e9ffb29dac7cb8da51f8bc7484f19864b7b9
Zain Kizilbash <56188371+ZainKizilbash@users.noreply.github.com>
2021-10-12 13:43:19 +0500

    Marksheet and result.

--- Q5.c
+++ Q5.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	float velocity;
-	float acceleration;
-	float time;
-	float distance;
-	printf("Initial velocity:  ");
-	scanf("%f", &velocity);
-	printf("Acceleration: ");
-	scanf("%f", &acceleration);
-	printf("Time: ");
-	scanf("%f", &time);
-	distance=velocity*time+0.5*acceleration*time*time;
-	printf("Distance covered: %0.4f m",distance);
-	return 0;
+float PF,Maths,Eng,Physics,Ict,percent;
+printf("Enter your PF marks: ");
+scanf("%f",&PF);
+printf("Enter your Maths marks: ");
+scanf("%f",&Maths);
+printf("Enter your English marks: ");
+scanf("%f",&Eng);
+printf("Enter your Physics marks: ");
+scanf("%f",&Physics);
+printf("Enter your ICT marks: ");
+scanf("%f",&Ict);
+percent=(Physics+Ict+Eng+Maths+PF)/500*100;
+if(percent<50||PF<40||Maths<40||Eng<40||Physics<40||Ict<40){
+printf("You have failed this course with a percentage of %0.2f%%",percent);
+}
+else{
+printf("You have passed this course with a percentage of %0.2f%%",percent);
+}
+return 0;
 }
